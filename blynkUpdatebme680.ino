bf9b123099d1f8f692332345f897b5121b155340
Ruqhia <53549229+ruqhia@users.noreply.github.com>
2022-02-20 02:48:06 -0500

    Create blynkUpdatebme680.ino

---
+++ blynkUpdatebme680.ino
@@ -0,0 +1,146 @@
+/*************************************************************
+  Download latest Blynk library here:
+    https://github.com/blynkkk/blynk-library/releases/latest
+
+  Blynk is a platform with iOS and Android apps to control
+  Arduino, Raspberry Pi and the likes over the Internet.
+  You can easily build graphic interfaces for all your
+  projects by simply dragging and dropping widgets.
+
+    Downloads, docs, tutorials: http://www.blynk.cc
+    Sketch generator:           http://examples.blynk.cc
+    Blynk community:            http://community.blynk.cc
+    Follow us:                  http://www.fb.com/blynkapp
+                                http://twitter.com/blynk_app
+
+  Blynk library is licensed under MIT license
+  This example code is in public domain.
+
+ *************************************************************
+
+  This example shows how value can be pushed from Arduino to
+  the Blynk App.
+
+  NOTE:
+  BlynkTimer provides SimpleTimer functionality:
+    http://playground.arduino.cc/Code/SimpleTimer
+
+  App project setup:
+    Value Display widget attached to Virtual Pin V5
+ *************************************************************/
+
+/* Comment this out to disable prints and save space */
+#define BLYNK_PRINT Serial
+
+/* Fill-in your Template ID (only if using Blynk.Cloud) */
+#define BLYNK_PRINT Serial
+#define BLYNK_TEMPLATE_ID "TMPL7E44f4L-"
+#define BLYNK_DEVICE_NAME "Quickstart Template"
+#include <SPI.h>
+#include "Zanshin_BME680.h"  // Include the BME680 Sensor library
+#include <WiFiNINA.h>
+#include <BlynkSimpleWiFiNINA.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "bB8W5lgdag8KjQfDt4bf5NFnFyuTpiPY";
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "SM-G950W2786";
+char pass[] = "qyss0818";
+BlynkTimer timer;
+const uint32_t SERIAL_SPEED{115200};  ///< Set the baud rate for Serial I/O
+
+/**************************************************************************************************
+** Declare global variables and instantiate classes                                              **
+**************************************************************************************************/
+BME680_Class BME680;  ///< Create an instance of the BME680 class
+///< Forward function declaration with default value for sea level
+float altitude(const int32_t press, const float seaLevel = 1013.25);
+float altitude(const int32_t press, const float seaLevel) {
+  /*!
+  @brief     This converts a pressure measurement into a height in meters
+  @details   The corrected sea-level pressure can be passed into the function if it is known,
+             otherwise the standard atmospheric pressure of 1013.25hPa is used (see
+             https://en.wikipedia.org/wiki/Atmospheric_pressure) for details.
+  @param[in] press    Pressure reading from BME680
+  @param[in] seaLevel Sea-Level pressure in millibars
+  @return    floating point altitude in meters.
+  */
+  static float Altitude;
+  Altitude =
+      44330.0 * (1.0 - pow(((float)press / 100.0) / seaLevel, 0.1903));  // Convert into meters
+  return (Altitude);
+}  // of method altitude()
+
+
+// This function sends Arduino's up time every second to Virtual Pin (5).
+// In the app, Widget's reading frequency should be set to PUSH. This means
+// that you define how often to send data to Blynk App.
+void myTimerEvent()
+{
+  static int32_t  temp, humidity, pressure, gas;  // BME readings
+  BME680.getSensorData(temp, humidity, pressure, gas);  // Get readings
+  int temperature = temp/100;
+  humidity = humidity % 1000;
+  pressure = pressure % 100;
+  if (temperature > 20)
+  {
+   Blynk.logEvent("temperature","Temperature is high");
+  }
+  // if (humidity > 20)
+  // {
+  //  Blynk.logEvent("humidity, temperature");
+  // }
+  // if (pressure > 20)
+  // {
+  //  Blynk.logEvent("pressure, temperature");
+  // }
+  Blynk.virtualWrite(V5, temperature);
+  Blynk.virtualWrite(V7, humidity);
+  Blynk.virtualWrite(V6, pressure);  
+}
+
+void setup()
+{
+  // Debug console
+  Serial.begin(115200);
+
+  Blynk.begin(auth, ssid, pass);
+   /*!
+  @brief    Arduino method called once at startup to initialize the system
+  @details  This is an Arduino IDE method which is called first upon boot or restart. It is only
+            called one time and then control goes to the main "loop()" method, from which
+            control never returns
+  @return   void
+  */
+  Serial.begin(SERIAL_SPEED);  // Start serial port at Baud rate
+#ifdef __AVR_ATmega32U4__      // If this is a 32U4 processor, then wait 3 seconds to init USB port
+  delay(3000);
+#endif
+  Serial.print(F("Starting I2CDemo example program for BME680\n"));
+  Serial.print(F("- Initializing BME680 sensor\n"));
+  while (!BME680.begin(I2C_STANDARD_MODE)) {  // Start BME680 using I2C, use first device found
+    Serial.print(F("-  Unable to find BME680. Trying again in 5 seconds.\n"));
+    delay(5000);
+  }  // of loop until device is located
+  Serial.print(F("- Setting 16x oversampling for all sensors\n"));
+  BME680.setOversampling(TemperatureSensor, Oversample16);  // Use enumerated type values
+  BME680.setOversampling(HumiditySensor, Oversample16);     // Use enumerated type values
+  BME680.setOversampling(PressureSensor, Oversample16);     // Use enumerated type values
+  Serial.print(F("- Setting IIR filter to a value of 4 samples\n"));
+  BME680.setIIRFilter(IIR4);  // Use enumerated type values
+  Serial.print(F("- Setting gas measurement to 320\xC2\xB0\x43 for 150ms\n"));  // "�C" symbols
+  BME680.setGas(320, 150);  // 320�c for 150 milliseconds
+  
+  // Setup a function to be called every second
+  timer.setInterval(1000L, myTimerEvent);
+}
+
+void loop()
+{
+  Blynk.run();
+  timer.run(); // Initiates BlynkTimer
+}
+
